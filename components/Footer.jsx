3099e8eb5d11cef9a87c2703fe485e1bad4c420c
John Johnson <jjmbeba@gmail.com>
2022-11-03 05:29:43 +0300

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
+
+const Footer = () => {
+
+  const year = new Date().getFullYear()
+
+  return (
+    <div className='footer-container'>
+      <p>{year} BeatIt. Headphones All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
